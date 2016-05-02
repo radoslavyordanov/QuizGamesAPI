44a684b4495b7b8905d2984e195f8adb6468247a
radoslavyordanov <radoslav@kodovpotok.bg>
2016-05-02 10:40:23 +0300

    Update API.

--- REST.js
+++ REST.js
@@ -43,7 +43,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 	});
 
 	router.get("/results/cars",function(req,res){
-		var options = {sql: 'SELECT * FROM results WHERE type="cars" ORDER BY score DESC;', nestTables: false};
+		var options = {sql: 'SELECT results.id, results.user_id, results.score, results.date, results.type, username as name FROM results INNER JOIN users ON results.user_id = users.id WHERE type="cars" ORDER BY score DESC;', nestTables: false};
 		connection.query(options,function(err,rows){
 			if(err) {
 				res.json({"Error" : true, "Message" : "Error executing MySQL query"});
@@ -55,7 +55,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 
 	router.post("/results/cars",function(req,res){
 		var query = "INSERT INTO ??(??,??, ??) VALUES (?,?,?)";
-		var table = ["results","name","score", "type",req.body.name,req.body.score, "cars"];
+		var table = ["results","user_id","score", "type",req.body.user_id,req.body.score, "cars"];
 		query = mysql.format(query,table);
 		connection.query(query,function(err,rows){
 			if(err) {
@@ -67,7 +67,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 	});
 
 	router.get("/results/logos",function(req,res){
-		var options = {sql: 'SELECT * FROM results WHERE type="logos" ORDER BY score DESC;', nestTables: false};
+		var options = {sql: 'SELECT results.id, results.user_id, results.score, results.date, results.type, username as name FROM results INNER JOIN users ON results.user_id = users.id WHERE type="logos" ORDER BY score DESC;', nestTables: false};
 		connection.query(options,function(err,rows){
 			if(err) {
 				res.json({"Error" : true, "Message" : "Error executing MySQL query"});
@@ -79,7 +79,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 
 	router.post("/results/logos",function(req,res){
 		var query = "INSERT INTO ??(??,??, ??) VALUES (?,?,?)";
-		var table = ["results","name","score", "type",req.body.name,req.body.score, "logos"];
+		var table = ["results","user_id","score", "type",req.body.user_id,req.body.score, "logos"];
 		query = mysql.format(query,table);
 		connection.query(query,function(err,rows){
 			if(err) {
@@ -91,7 +91,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 	});
 
 	router.get("/results/cities",function(req,res){
-		var options = {sql: 'SELECT * FROM results WHERE type="cities" ORDER BY score DESC;', nestTables: false};
+		var options = {sql: 'SELECT results.id, results.user_id, results.score, results.date, results.type, username as name FROM results INNER JOIN users ON results.user_id = users.id WHERE type="cities" ORDER BY score DESC;', nestTables: false};
 		connection.query(options,function(err,rows){
 			if(err) {
 				res.json({"Error" : true, "Message" : "Error executing MySQL query"});
@@ -103,7 +103,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 
 	router.post("/results/cities",function(req,res){
 		var query = "INSERT INTO ??(??,??, ??) VALUES (?,?,?)";
-		var table = ["results","name","score", "type",req.body.name,req.body.score, "cities"];
+		var table = ["results","user_id","score", "type",req.body.user_id,req.body.score, "cities"];
 		query = mysql.format(query,table);
 		connection.query(query,function(err,rows){
 			if(err) {
@@ -115,7 +115,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 	});
 
 	router.get("/results/all",function(req,res){
-		var options = {sql: 'SELECT * FROM results ORDER BY score DESC;', nestTables: false};
+		var options = {sql: 'SELECT results.id, results.user_id, results.score, results.date, results.type, username as name FROM results INNER JOIN users ON results.user_id = users.id ORDER BY score DESC;', nestTables: false};
 		connection.query(options,function(err,rows){
 			if(err) {
 				res.json({"Error" : true, "Message" : "Error executing MySQL query"});
@@ -125,6 +125,109 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
 		});
 	});
 
+	router.post("/login", function(req,res) {
+		var query = "SELECT COUNT(*) as result, username, password, id, user_roleId from users WHERE username=? AND password=?;";
+		var params = [req.body.username, req.body.password];
+		query = mysql.format(query,params);
+		connection.query(query,function(err,rows){
+			if(err) {
+				res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+			} else {
+				if (rows[0].result == 1) {
+					res.json({"status" : "success", "id": rows[0].id, "username" : rows[0].username, "password": rows[0].password, "user_roleId": rows[0].user_roleId});
+				} else {
+					res.json({"status" : "failure"});
+				}
+			}
+		});
+	});
+
+	router.post("/register", function(req,res) {
+		var checkQuery = "SELECT COUNT(*) as result from users WHERE username=?;";
+		var params = [req.body.username];
+		checkQuery = mysql.format(checkQuery,params);
+		connection.query(checkQuery,function(err,rows){
+			if(err) {
+				res.json({"Error" : true, "Message" : "Error executing register checkQuery"});
+			} else {
+				if (rows[0].result == 1) {
+					res.json({"status" : "failure"});
+				} else {
+					var insertQuery = "INSERT INTO ??(??,??,??) VALUES(?,?,2);"
+					var params = ["users","username","password","user_roleId",req.body.username, req.body.password];
+					insertQuery = mysql.format(insertQuery,params);
+					connection.query(insertQuery,function(err,rows){
+						if(err) {
+							res.json({"Error" : true, "Message" : "Error executing register insertQuery query"});
+						} else {
+							var selectQuery = "SELECT ??, ??, ?? FROM ?? WHERE username=? AND password=?;"
+							var params = ["id", "username","password","users",req.body.username, req.body.password];
+							selectQuery = mysql.format(selectQuery,params);
+							connection.query(selectQuery,function(err,rows){
+								if(err) {
+									res.json({"Error" : true, "Message" : "Error executing register selectQuery query"});
+								} else {
+									res.json({"status" : "success", "id": rows[0].id, "username" : rows[0].username, "password": rows[0].password});
+								}
+							});
+						}
+					});	
+				}
+			}
+		});
+
+	});
+
+	router.post("/addQuiz", function(req,res) {
+		var insertQuizQuery = "INSERT INTO ??(??, ??) VALUES (?, ?);"
+		var params = ["quiz", "quiz_image", "quiz_type", req.body.quizImage, req.body.quizType];
+		insertQuizQuery = mysql.format(insertQuizQuery,params);
+		connection.query(insertQuizQuery,function(err,rows){
+			if(err) {
+				res.json({"Error" : true, "Message" : "Error inserting addQuiz"});
+			} else { 
+				var insertQuizChoices1 = "INSERT INTO ??(??, ??, ??) VALUES (?, ?, ?);"
+				var params1 = ["quiz_choices", "quiz_id", "choice", "is_right_choice", rows.insertId, req.body.quizChoices[0].choice, req.body.quizChoices[0].isRightChoice.toString()];
+				insertQuizChoices1 = mysql.format(insertQuizChoices1,params1);
+				
+				connection.query(insertQuizChoices1,function(err,rows){
+					if(err) {
+						res.json({"Error" : true, "Message" : "Error inserting addQuiz"});
+					} else { 
+						if (req.body.quizChoices.length == 1){
+							res.json({"status" : "success"});
+						}
+					}
+				});
+
+				if (req.body.quizChoices.length == 4) {
+					var insertQuizChoices2 = "INSERT INTO ??(??, ??, ??) VALUES (?, ?, ?);"
+					var params2 = ["quiz_choices", "quiz_id", "choice", "is_right_choice", rows.insertId, req.body.quizChoices[1].choice, req.body.quizChoices[1].isRightChoice.toString()];
+					var insertQuizChoices2 = mysql.format(insertQuizChoices2, params2);
+
+					var insertQuizChoices3= "INSERT INTO ??(??, ??, ??) VALUES (?, ?, ?);"
+					var params3 = ["quiz_choices", "quiz_id", "choice", "is_right_choice", rows.insertId, req.body.quizChoices[2].choice, req.body.quizChoices[2].isRightChoice.toString()];
+					var insertQuizChoices3 = mysql.format(insertQuizChoices3,params3);
+
+					var insertQuizChoices4 = "INSERT INTO ??(??, ??, ??) VALUES (?, ?, ?);"
+					var params4 = ["quiz_choices", "quiz_id", "choice", "is_right_choice", rows.insertId, req.body.quizChoices[3].choice, req.body.quizChoices[3].isRightChoice.toString()];
+					var insertQuizChoices4 = mysql.format(insertQuizChoices4,params4);
+
+					var insertQuizChoices = insertQuizChoices2 + insertQuizChoices3 + insertQuizChoices4;
+					connection.query(insertQuizChoices,function(err,rows){
+						if(err) {
+							res.json({"Error" : true, "Message" : "Error inserting addQuiz"});
+						} else { 
+							res.json({"status" : "success"});
+						}
+					});
+
+				}
+
+			}
+
+		});
+	});
 
 }
 
